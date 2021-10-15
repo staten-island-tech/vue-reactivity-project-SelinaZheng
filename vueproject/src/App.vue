a8e0df66703b1108d316a2ae9e518e8c4c6e8530
SelinaZheng <71414455+SelinaZheng@users.noreply.github.com>
2021-10-15 12:28:46 -0400

    restart

---
+++ vueproject/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <div class="nav-bar"></div>
+    <div class="flex">
+      <div class="image">
+        <img v-bind:src="image" v-bind:alt="alt">
+      </div>
+      <div class="colors">
+
+      </div>
+      <div class="cart">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+
+  },
+  data(){
+    return{
+      image:"./assets/flower.jpg",
+      alt:,
+    };
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
